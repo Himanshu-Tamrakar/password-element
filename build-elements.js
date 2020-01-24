47030fd16b6baf1b70ec4b8480622955d88e8f57
Himanshu Tamrakar <himanshu.tamrakar@decimal.co.in>
2020-01-24 11:12:14 +0530

    element1

--- build-elements.js
+++ build-elements.js
@@ -14,22 +14,25 @@ fs.readdir("./dist/password-element", function(err, items) {
 });
 
  function build(files) {
-   console.log(files)
-  // const files = [
-  //   './dist/password-element/scripts.9ddf727ecc94cd123172.js',
-  //   './dist/password-element/runtime-es2015.f80f43fdfe5b65e71027.js',
-  //   './dist/password-element/runtime-es5.b955bc8e9160c78c7a86.js',
-
-  //   './dist/password-element/polyfills-es2015.15a782f8537150de6053.js',
-  //   './dist/password-element/polyfills-es5.ae9d6bbf477df79fb45f.js',
-
-  //   './dist/password-element/main-es2015.8a1a2f7f76f2a2c71301.js',
-  //   './dist/password-element/main-es5.53c45a4828b018d64634.js',
-  // ]
-  
   fs.ensureDir('elements')
   concat(files, 'elements/elements.js');
   if(css != "") fs.copyFile(css, 'elements/styles.css')
   //await fs.copy('./dist/elements/assets/', 'elements/assets/' )
 
+
+  const srcpath = '/home/himanshu/Learning/password-element/elements/elements.js'
+  const dstpath = '/home/himanshu/Learning/password-element-use/src/assets/password/elements.js'
+  //With a callback:
+  fs.move(srcpath, dstpath, err => {
+  if (err) return console.error(err)
+    console.log('success!')
+  })
+
+  const srcpath1 = '/home/himanshu/Learning/password-element/elements/styles.css'
+  const dstpath1 = '/home/himanshu/Learning/password-element-use/src/assets/password/styles.css'
+  //With a callback:
+  fs.move(srcpath1, dstpath1, err => {
+    if (err) return console.error(err)
+    console.log('element move to destination folder!')
+  })
 }
